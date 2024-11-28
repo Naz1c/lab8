87f111c55422ac3a3960636418096e412fb94af1
Naz1c <nazacep@gmail.com>
2024-11-28 07:58:50 +0200

    Add files via upload

---
+++ freshcar/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+}
+
+function toggleFilter() {
+    const filter = document.getElementById('filter');
+    filter.classList.toggle('active');
+}
